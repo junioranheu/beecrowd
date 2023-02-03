b6518ecc589262f5fa7d9deb232004f4b43ed2b6
junioranheu <junioranheu@gmail.com>
2023-02-03 15:41:02 -0300

    desafio 1011

--- 1011-esfera/Program.cs
+++ 1011-esfera/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1011
+int raio = Convert.ToInt32(Console.ReadLine());
+double pi = 3.14159;
+double volume = (4 / 3.0) * pi * (Math.Pow(raio, 3));
+
+Console.WriteLine($"VOLUME = {volume:F3}");
