865464f4be151001841e102ad5d42552a53fa6b3
junioranheu <junioranheu@gmail.com>
2023-02-03 14:50:37 -0300

    desafio 1006

---
+++ 1006-media-2/Program.cs
@@ -0,0 +1,9 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1006
+double A = Convert.ToDouble(Console.ReadLine());
+double B = Convert.ToDouble(Console.ReadLine());
+double C = Convert.ToDouble(Console.ReadLine());
+double peso1 = 2;
+double peso2 = 3;
+double peso3 = 5;
+double MEDIA = ((A * peso1) + (B * peso2) + (C * peso3)) / (peso1 + peso2 + peso3);
+Console.WriteLine($"MEDIA = {MEDIA:F1}");
