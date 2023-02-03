c0aa85d239fcb68d47598c013dadcf7ccf3a092f
junioranheu <junioranheu@gmail.com>
2023-02-03 19:21:09 -0300

    desafio 1016

--- 1015-distancia-entre-dois-pontos/Program.cs
+++ 1015-distancia-entre-dois-pontos/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1015
+string[] valores1 = Console.ReadLine().Split(' ');
+double x1 = double.Parse(valores1[0]);
+double y1 = double.Parse(valores1[1]);
+
+string[] valores2 = Console.ReadLine().Split(' ');
+double x2 = double.Parse(valores2[0]);
+double y2 = double.Parse(valores2[1]);
+
+double final = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+Console.WriteLine($"{final:F4}");
