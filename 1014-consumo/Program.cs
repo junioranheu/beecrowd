10845fdd6a20efc11adb2a85928ea894fb1f3074
junioranheu <junioranheu@gmail.com>
2023-02-03 19:14:19 -0300

    desafio 1014

--- 1014-consumo/Program.cs
+++ 1014-consumo/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1014
+int X = Convert.ToInt32(Console.ReadLine());
+double Y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"{(X / Y):F3} km/l");
