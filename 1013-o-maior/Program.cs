a994584b8a317c0503551f811088c8a201a004c2
junioranheu <junioranheu@gmail.com>
2023-02-03 19:06:30 -0300

    desafio 1013

--- 1013-o-maior/Program.cs
+++ 1013-o-maior/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1013
+string[] valores = Console.ReadLine().Split(' ');
+int A = Convert.ToInt32(valores[0]);
+int B = Convert.ToInt32(valores[1]);
+int C = Convert.ToInt32(valores[2]);
+
+int maiorAB = (A + B + Math.Abs(A - B)) / 2;
+int maiorMaiorC = (maiorAB+ C + Math.Abs(maiorAB - C)) / 2;
+
+Console.WriteLine($"{maiorMaiorC} eh o maior");
