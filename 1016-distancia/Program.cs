786e4e42ac936e572dd4fa14b19c24d684ebdd21
junioranheu <junioranheu@gmail.com>
2023-02-03 19:43:56 -0300

    desafio 1017

--- 1016-distancia/Program.cs
+++ 1016-distancia/Program.cs
@@ -1,1 +1,5 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1016
+const int kmPorMinuto = 2;
+int km = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{(kmPorMinuto * km)} minutos");
--- 1017-gasto-de-combustivel/Program.cs
+++ 1017-gasto-de-combustivel/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1017
+const int kmPorLitro = 12;
+double tempoGastoViagemEmHoras = int.Parse(Console.ReadLine());
+double velocidadeMedia = int.Parse(Console.ReadLine());
+
+double r = (tempoGastoViagemEmHoras * velocidadeMedia) / kmPorLitro;
+
+Console.WriteLine($"{r:F3}");
