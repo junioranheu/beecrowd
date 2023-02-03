ddd5664ff7a43c0e0db87735bbb0c0d26b697be4
junioranheu <junioranheu@gmail.com>
2023-02-03 14:54:53 -0300

    desafio 1007

---
+++ 1007-diferenca/Program.cs
@@ -0,0 +1,7 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1007
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+int D = Convert.ToInt32(Console.ReadLine());
+int DIFERENCA = (A * B) - (C *D);
+Console.WriteLine($"DIFERENCA = {DIFERENCA}");
