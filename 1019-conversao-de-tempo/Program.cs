4bdc314f921c145c9af6feecfd2ce05f96fcf3dd
junioranheu <junioranheu@gmail.com>
2023-02-03 20:47:24 -0300

    desafio 1019

--- 1019-conversao-de-tempo/Program.cs
+++ 1019-conversao-de-tempo/Program.cs
@@ -1,1 +1,15 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1019
+int valor = int.Parse(Console.ReadLine());
+
+int horasEmSegundos = 60 * 60;
+int horas = valor / horasEmSegundos;
+valor -= horas * horasEmSegundos;
+
+int minutosEmSegundos = 60;
+int minutos = valor / minutosEmSegundos;
+valor -= minutos * minutosEmSegundos;
+
+int segundosEmSegundos = 1;
+int segundos = valor / segundosEmSegundos;
+
+Console.WriteLine($"{horas}:{minutos}:{segundos}");
