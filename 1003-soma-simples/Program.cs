570b635c89de10855e6d21cb92e3d9b0f46880bd
junioranheu <junioranheu@gmail.com>
2023-02-03 14:35:34 -0300

    desafio 1004

--- 1003-soma-simples/Program.cs
+++ 1003-soma-simples/Program.cs
@@ -1,4 +1,5 @@
-﻿int A = Convert.ToInt32(Console.ReadLine());
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1003
+int A = Convert.ToInt32(Console.ReadLine());
 int B = Convert.ToInt32(Console.ReadLine());
 int SOMA = A + B;
 Console.WriteLine($"SOMA = {SOMA}");
---
+++ 1004-produtos-simples/Program.cs
@@ -0,0 +1,5 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1004
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int PROD = A * B;
+Console.WriteLine($"PROD = {PROD}");
