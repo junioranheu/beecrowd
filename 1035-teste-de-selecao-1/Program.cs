10795995e2645d08e02a82052dac8c4c13e2998f
junioranheu <junioranheu@gmail.com>
2023-02-04 01:11:57 -0300

    desafio 1035

--- 1035-teste-de-selecao-1/Program.cs
+++ 1035-teste-de-selecao-1/Program.cs
@@ -1,4 +1,17 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- 
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1035
+string[] valores = Console.ReadLine().Split(' ');
+
+int A = Convert.ToInt32(valores[0]);
+int B = Convert.ToInt32(valores[1]);
+int C = Convert.ToInt32(valores[2]);
+int D = Convert.ToInt32(valores[3]);
+
+if ((B > C) && (D > A) && ((C + D) > (A +B)) && (C > 0) && (D > 0) && (A % 2 == 0))
+{
+    Console.WriteLine("Valores aceitos");
+}
+else
+{
+    Console.WriteLine("Valores nao aceitos");
+}
+
