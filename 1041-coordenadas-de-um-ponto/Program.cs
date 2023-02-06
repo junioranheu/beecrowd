5557e43e1a5dab6f1edff461361644c82f808f45
junioranheu <junioranheu@gmail.com>
2023-02-05 23:19:28 -0300

    iniciar desafio 1041

--- 1041-coordenadas-de-um-ponto/Program.cs
+++ 1041-coordenadas-de-um-ponto/Program.cs
@@ -1,1 +1,25 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1041
+string[] valores = Console.ReadLine().Split(' ');
+
+double x = double.Parse(valores[0]);
+double y = double.Parse(valores[1]);
+
+if (x == 0 && y == 0)
+{
+    Console.WriteLine("Origem");
+}
+else if (x == 0 && y != 0)
+{
+    Console.WriteLine("Eixo Y");
+}
+else if (y == 0 && x != 0)
+{
+    Console.WriteLine("Eixo X");
+}
+else
+{
+    int trunc = (int)Math.Truncate(x);
+    trunc = (trunc < 1) ? 1 : trunc;
+
+    Console.WriteLine($"Q{trunc}");
+}
