740f3d83354e08f14356febdf093600d567e06c7
junioranheu <junioranheu@gmail.com>
2023-02-06 22:22:32 -0300

    desafio 1042

--- 1042-sort-simples/Program.cs
+++ 1042-sort-simples/Program.cs
@@ -1,16 +1,18 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1042
 string[] lista = Console.ReadLine().Split(' ');
 string[] listaOriginal = DeepCopy(lista);
+int lenghtMenos2 = lista.Length - 2;
 
-for (int i = 0; i <= lista.Length - 2; i++)
+for (int i = 0; i <= lenghtMenos2; i++)
 {
-    for (int j = 0; j <= lista.Length - 2; j++)
+    for (int j = 0; j <= lenghtMenos2; j++)
     {
+        // Se o valor do loop atual (lista[j]) for maior que o próximo (lista[j + 1])...
         if (int.Parse(lista[j]) > int.Parse(lista[j + 1]))
         {
-            int temp = int.Parse(lista[j + 1]);
-            lista[j + 1] = lista[j];
-            lista[j] = temp.ToString();
+            string temp = lista[j + 1]; // "Jar" temporária do próximo valor;
+            lista[j + 1] = lista[j]; // Salvar o valor atual no lugar do próximo
+            lista[j] = temp; // "Empurrar" o valor que foi salvo na temp para o index atual;
         }
     }
 }
