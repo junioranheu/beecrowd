89bc681b2196a8e435698f11e1b4c9f9ef86a12d
junioranheu <junioranheu@gmail.com>
2023-02-08 18:45:56 -0300

    desafio 1046

--- 1046-tempo-de-jogo/Program.cs
+++ 1046-tempo-de-jogo/Program.cs
@@ -1,1 +1,23 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1046
+string[] horas = Console.ReadLine().Split(' ');
+
+int inicio = int.Parse(horas[0]);
+int fim = int.Parse(horas[1]);
+int diferenca = 0;
+
+if (inicio == 0 && fim == 0)
+{
+    diferenca = 24;
+}
+else if (inicio < fim)
+{
+    diferenca = inicio - fim;
+    diferenca = diferenca - (diferenca * 2);
+}
+else
+{
+    int ateMeiaNoite = 24 - inicio;
+    diferenca = ateMeiaNoite + fim;
+}
+
+Console.WriteLine($"O JOGO DUROU {diferenca} HORA(S)");
