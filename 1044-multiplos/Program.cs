1f40b63810c3535abe9870275a0bcd99a882787a
junioranheu <junioranheu@gmail.com>
2023-02-07 21:00:33 -0300

    desafio 1044

--- 1044-multiplos/Program.cs
+++ 1044-multiplos/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1044
+string[] valores = Console.ReadLine().Split(' ');
+
+int A = int.Parse(valores[0]);
+int B = int.Parse(valores[1]);
+
+bool isMutiplo = A % B == 0 || B % A == 0;
+
+Console.WriteLine(isMutiplo ? "Sao Multiplos" : "Nao sao Multiplos");
