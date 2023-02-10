4214e6b7c857f54c82f5a7e909c5c51676fe2ebd
junioranheu <junioranheu@gmail.com>
2023-02-10 19:30:54 -0300

    desafio 1051

--- 1051-imposto-de-renda/Program.cs
+++ 1051-imposto-de-renda/Program.cs
@@ -1,1 +1,38 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1051
+double salario = double.Parse(Console.ReadLine());
+
+if (salario <= 2000)
+{
+    Console.WriteLine("Isento");
+}
+else
+{
+    Console.WriteLine($"R$ {ChecarImpostoDeRenda(salario):F2}");
+}
+
+static double ChecarImpostoDeRenda(double salario)
+{
+    double impostoTotal = 0;
+
+    if (salario >= 2000.01)
+    {
+        double maximo = salario < 3000 ? salario : 3000;
+        double imposto = ((maximo - 2000) * (8.0 / 100.0));
+        impostoTotal += imposto;
+    }
+
+    if (salario >= 3000.01)
+    {
+        double maximo = salario < 4500 ? salario : 4500;
+        double imposto = ((maximo - 3000) * (18.0 / 100.0));
+        impostoTotal += imposto;
+    }
+
+    if (salario >= 4500.01)
+    {
+        double imposto = ((salario - 4500) * (28.0 / 100.0));
+        impostoTotal += imposto;
+    }
+
+    return impostoTotal;
+}
