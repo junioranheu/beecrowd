60d94d086bfc7497d494a594fcbd72d09dd4814f
junioranheu <junioranheu@gmail.com>
2023-02-10 18:57:11 -0300

    desafio 1048

--- 1048-aumento-de-salario/Program.cs
+++ 1048-aumento-de-salario/Program.cs
@@ -1,1 +1,32 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1048
+double salario = double.Parse(Console.ReadLine());
+
+double percentualReajuste = VerificarPercentualReajuste(salario);
+
+Console.WriteLine($"Novo salario: {(salario + (salario * (percentualReajuste / 100))):F2}");
+Console.WriteLine($"Reajuste ganho: {(salario * (percentualReajuste / 100)):F2}");
+Console.WriteLine($"Em percentual: {percentualReajuste} %");
+
+static double VerificarPercentualReajuste(double salario)
+{
+    if (salario >= 0 && salario <= 400)
+    {
+        return 15;
+    }
+    else if (salario >= 400.01 && salario <= 800)
+    {
+        return 12;
+    }
+    else if (salario >= 800.01 && salario <= 1200)
+    {
+        return 10;
+    }
+    else if (salario >= 1200.01 && salario <= 2000)
+    {
+        return 7;
+    }
+    else
+    {
+        return 4;
+    }
+}
