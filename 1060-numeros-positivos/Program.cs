e23a9b9ff55c466f3ce26bec5bffd33586bbe04c
junioranheu <junioranheu@gmail.com>
2023-02-12 11:29:48 -0300

    desafio 1060

--- 1060-numeros-positivos/Program.cs
+++ 1060-numeros-positivos/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1060
+double valor1 = double.Parse(Console.ReadLine());
+double valor2 = double.Parse(Console.ReadLine());
+double valor3 = double.Parse(Console.ReadLine());
+double valor4 = double.Parse(Console.ReadLine());
+double valor5 = double.Parse(Console.ReadLine());
+double valor6 = double.Parse(Console.ReadLine());
+
+double[] valores = new double[] { valor1, valor2, valor3, valor4, valor5, valor6 };
+
+Console.WriteLine($"{ChecarPositivos(valores)} valores positivos");
+
+static int ChecarPositivos(double[] valores)
+{
+    int positivos = 0;
+
+    for (int i = 0; i < valores.Length; i++)
+    {
+        if (valores[i] >= 0)
+        {
+            positivos++;
+        }
+    }
+
+    return positivos;
+}
