3f91759d82a63b2adcc91734a0016c5913916cde
junioranheu <junioranheu@gmail.com>
2023-02-12 11:20:19 -0300

    desafio 1052 & 1059

--- 1059-numeros-pares/Program.cs
+++ 1059-numeros-pares/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1059
+for(int i = 1; i <= 100; i++)
+{
+    if (IsPar(i))
+    {
+        Console.WriteLine(i);
+    } 
+}
+
+static bool IsPar(int i )
+{
+    return i % 2 == 0;
+}
