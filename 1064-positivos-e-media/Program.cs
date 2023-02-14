739f85fff39836f69f7fc65dd33cc4f8dec2364b
junioranheu <junioranheu@gmail.com>
2023-02-14 18:50:24 -0300

    desafio 1064

--- 1064-positivos-e-media/Program.cs
+++ 1064-positivos-e-media/Program.cs
@@ -1,1 +1,18 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1064
+const int qtdLoops = 6;
+int qtdValoresPositivos = 0;
+double mediaValoresPositivos = 0;
+
+for (int i = 0; i < qtdLoops; i++)
+{
+    double valor = double.Parse(Console.ReadLine());
+
+    if (valor > 0)
+    {
+        qtdValoresPositivos += 1;
+        mediaValoresPositivos += valor;
+    }
+}
+
+Console.WriteLine($"{qtdValoresPositivos} valores positivos");
+Console.WriteLine($"{(mediaValoresPositivos/ qtdValoresPositivos):F1}");
