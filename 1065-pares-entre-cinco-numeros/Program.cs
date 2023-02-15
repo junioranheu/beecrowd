534dea122f8021f5fd2d650b5bae744c1b2f1b86
junioranheu <junioranheu@gmail.com>
2023-02-14 21:16:50 -0300

    desafio 1065

--- 1065-pares-entre-cinco-numeros/Program.cs
+++ 1065-pares-entre-cinco-numeros/Program.cs
@@ -1,1 +1,15 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1065
+const int qtdLoops = 5;
+int numerosPares = 0;
+
+for (int i = 0; i < qtdLoops; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+
+    if (n % 2 == 0)
+    {
+        numerosPares += 1;
+    }
+}
+
+Console.WriteLine($"{numerosPares} valores pares");
