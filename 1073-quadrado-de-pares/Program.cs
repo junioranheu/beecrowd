c41d0232ac2582e788e1cd895cc063d618c22ca4
junioranheu <junioranheu@gmail.com>
2023-02-14 22:08:21 -0300

    desafio 1073

--- 1073-quadrado-de-pares/Program.cs
+++ 1073-quadrado-de-pares/Program.cs
@@ -1,1 +1,10 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1073
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= N; i++)
+{
+    if ((i % 2 == 0) && (i > 0))
+    {
+        Console.WriteLine($"{i}^{2} = {Math.Pow(i, 2)}");
+    }
+}
