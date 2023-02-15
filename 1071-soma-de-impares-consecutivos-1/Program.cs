4dcb8e906f8bd6ec6f7dfd4a3a398cc6f1c8dba2
junioranheu <junioranheu@gmail.com>
2023-02-14 21:52:36 -0300

    desafio 1071

--- 1071-soma-de-impares-consecutivos-1/Program.cs
+++ 1071-soma-de-impares-consecutivos-1/Program.cs
@@ -1,1 +1,23 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1071
+int X = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+
+int maior = X > Y ? X : Y;
+int menor = X < Y ? X : Y;
+
+int somaImpares = 0;
+
+for (int i = menor; i < maior; i++)
+{
+    if (IsImpar(i) && (i != menor) && (i != maior))
+    {
+        somaImpares += i;
+    }
+}
+
+Console.WriteLine(somaImpares);
+
+static bool IsImpar(int i)
+{
+    return i % 2 != 0;
+}
