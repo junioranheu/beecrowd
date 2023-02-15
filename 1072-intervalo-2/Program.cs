e54de1be310fdf0281cdd55d7a14c3e6fe352d7a
junioranheu <junioranheu@gmail.com>
2023-02-14 22:02:36 -0300

    desafio 1072

--- 1072-intervalo-2/Program.cs
+++ 1072-intervalo-2/Program.cs
@@ -1,1 +1,22 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1072
+int N = int.Parse(Console.ReadLine());
+
+int qtdIn = 0;
+int qtdOut = 0;
+
+for (int i = 0; i < N; i++)
+{
+    int X = int.Parse(Console.ReadLine());
+
+    if (X >= 10 && X <= 20)
+    {
+        qtdIn++;
+    }
+    else
+    {
+        qtdOut++;
+    }
+}
+
+Console.WriteLine($"{qtdIn} in");
+Console.WriteLine($"{qtdOut} out");
