16c00c8b3538ec64dd2d21808108c842ec46e220
junioranheu <junioranheu@gmail.com>
2023-02-14 21:37:32 -0300

    desafio 1067

--- 1067-numeros-impares/Program.cs
+++ 1067-numeros-impares/Program.cs
@@ -1,1 +1,10 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1067
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= x; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
