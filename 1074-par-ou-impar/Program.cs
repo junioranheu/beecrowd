1e75f6ccbf3e1085601a2f11160f2e5b3b2a99f2
junioranheu <junioranheu@gmail.com>
2023-02-15 20:12:28 -0300

    desafio 1074

--- 1074-par-ou-impar/Program.cs
+++ 1074-par-ou-impar/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1074
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    int X = int.Parse(Console.ReadLine());
+    Console.WriteLine(GerarResposta(X));
+}
+
+static string GerarResposta(int X)
+{
+    if (X == 0 )
+    {
+        return "NULL";
+    }
+
+    string evenOdd = X % 2 == 0 ? "EVEN" : "ODD";
+    string positiveNegative = X > 0 ? "POSITIVE" : "NEGATIVE";
+
+    return $"{evenOdd} {positiveNegative}";
+}
