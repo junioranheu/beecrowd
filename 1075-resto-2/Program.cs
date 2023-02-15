c7a95484789368888b76547f70769355b12ce6bf
junioranheu <junioranheu@gmail.com>
2023-02-15 20:15:30 -0300

    desafio 1075

--- 1075-resto-2/Program.cs
+++ 1075-resto-2/Program.cs
@@ -1,1 +1,10 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1075
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10000; i++)
+{
+    if (i % N == 2)
+    {
+        Console.WriteLine(i);
+    }
+}
