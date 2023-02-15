bdc602ce44c6302a1a3a1ea6324a1a7ebfc269ef
junioranheu <junioranheu@gmail.com>
2023-02-15 20:18:51 -0300

    desafio 1078

--- 1078-tabuada/Program.cs
+++ 1078-tabuada/Program.cs
@@ -1,1 +1,7 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1078
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{i} x {N} = {(i * N)}"); 
+}
