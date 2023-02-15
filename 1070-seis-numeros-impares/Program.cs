dcd2fd81678fb855186cb47a4a557f7b712c8eae
junioranheu <junioranheu@gmail.com>
2023-02-14 21:43:10 -0300

    desafio 1070

--- 1070-seis-numeros-impares/Program.cs
+++ 1070-seis-numeros-impares/Program.cs
@@ -1,1 +1,13 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1070
+int x = int.Parse(Console.ReadLine());
+const int limite = 6;
+int atual = 0;
+
+for (int i = x; i < x * 3; i++)
+{
+    if ((i % 2 != 0) && atual < limite)
+    {
+        Console.WriteLine(i);
+        atual++;
+    }
+}
