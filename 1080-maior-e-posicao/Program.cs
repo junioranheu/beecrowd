60f24be0d0499855a40743c0cd67f38a07e6e3d4
junioranheu <junioranheu@gmail.com>
2023-02-15 23:12:19 -0300

    desafio 1080

--- 1080-maior-e-posicao/Program.cs
+++ 1080-maior-e-posicao/Program.cs
@@ -1,1 +1,17 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1080
+int numeroMaior = 0;
+int indexNumeroMaior = 0;
+
+for (int i = 1; i <= 100; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+
+    if (n > numeroMaior)
+    {
+        numeroMaior = n;
+        indexNumeroMaior = i;
+    }
+}
+
+Console.WriteLine(numeroMaior);
+Console.WriteLine(indexNumeroMaior);
