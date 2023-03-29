7549e0859a698a8f57d36c36c9a69af88f82b6a9
junioranheu <junioranheu@gmail.com>
2023-03-29 20:36:05 -0300

    desafio 1113

--- 1113-crescente-e-decrescente/Program.cs
+++ 1113-crescente-e-decrescente/Program.cs
@@ -1,1 +1,15 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1113
+while (true)
+{
+    string[] numeros = Console.ReadLine().Split(' ');
+    int X = int.Parse(numeros[0]);
+    int Y = int.Parse(numeros[1]);
+
+    if (X == Y)
+    {
+        break;
+    }
+
+    string resultado = X > Y ? "Decrescente" : "Crescente";
+    Console.WriteLine(resultado);
+}
