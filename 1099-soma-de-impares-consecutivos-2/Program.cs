763539b168af34657ea2d70eb83f049e0a39dbb4
junioranheu <junioranheu@gmail.com>
2023-03-28 22:59:23 -0300

    desafio 1099

--- 1099-soma-de-impares-consecutivos-2/Program.cs
+++ 1099-soma-de-impares-consecutivos-2/Program.cs
@@ -1,1 +1,28 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1099
+int i = Convert.ToInt32(Console.ReadLine());
+
+for (int j = 0; j < i; j++)
+{
+    string[] numeros = Console.ReadLine().Split(' ');
+    int X = Convert.ToInt32(numeros[0]);
+    int Y = Convert.ToInt32(numeros[1]);
+
+    if (Y < X)
+    {
+        int jar = X;
+        X = Y;
+        Y = jar;
+    }
+
+    int somaImpares = 0;
+
+    for (int k = X; k < Y; k++)
+    {
+        if (k % 2 != 0 && k != X && k != Y)
+        {
+            somaImpares += k;
+        }
+    }
+
+    Console.WriteLine(somaImpares);
+}
