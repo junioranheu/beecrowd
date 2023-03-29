7aabe097552496dae272627ffb76baa91dc0ca40
junioranheu <junioranheu@gmail.com>
2023-03-28 21:59:13 -0300

    desafio 1096

--- 1096-sequencia-ij-2/Program.cs
+++ 1096-sequencia-ij-2/Program.cs
@@ -1,1 +1,18 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1096
+int i = 1, j = 7, cont = 1;
+
+while (i <= 9)
+{
+    Console.WriteLine($"I={i} J={j}");
+
+    cont++;
+
+    j -= 1;
+
+    if (cont > 3)
+    {
+        cont = 1;
+        i += 2;
+        j = 7;
+    }
+}
