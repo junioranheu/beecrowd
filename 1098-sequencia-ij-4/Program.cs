7584bd1dcbcf186d0bd960f3479d298cb60b9357
junioranheu <junioranheu@gmail.com>
2023-03-28 22:31:13 -0300

    desafio 1098

--- 1098-sequencia-ij-4/Program.cs
+++ 1098-sequencia-ij-4/Program.cs
@@ -1,1 +1,34 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1098
+double i = 0.0, j = 1.0;
+int cont = 0, contGeral = 0;
+
+while (i <= 2.0000000000000000000000001)
+{
+    if (i >= 1.9)
+    {
+        i = 2.0; // Bug fix;
+    }
+
+    Console.WriteLine($"I={NormalizarNumero(i)} J={NormalizarNumero(j)}");
+
+    j++;
+    cont++;
+
+    if (cont == 3)
+    {
+        contGeral++;
+        cont = 0;
+        i += 0.2;
+        j = 1 + (contGeral * 0.2);
+    }
+}
+
+static string NormalizarNumero(double d)
+{
+    if ((d % 1) == 0)
+    {
+        return $"{d:F0}";
+    }
+
+    return $"{d:F1}";
+}
