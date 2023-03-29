9fbe0fa5e4c2829fa8ef2fb217d2748fce01ccfe
junioranheu <junioranheu@gmail.com>
2023-03-29 19:59:57 -0300

    desafio 1101

--- 1101-sequencia-de-numeros-e-soma/Program.cs
+++ 1101-sequencia-de-numeros-e-soma/Program.cs
@@ -1,1 +1,31 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1101
+while (true)
+{
+    string[] numeros = Console.ReadLine().Split(' ');
+    int M = Convert.ToInt32(numeros[0]);
+    int N = Convert.ToInt32(numeros[1]);
+
+    if (M <= 0 || N <= 0)
+    {
+        break;
+    }
+
+    int menor = M;
+    int maior = N;
+
+    if (menor > maior)
+    {
+        menor = N;
+        maior = M;
+    }
+
+    string ordemFinal = "";
+    int somaFinal = 0;
+    for (int i = menor; i <= maior; i++)
+    {
+        ordemFinal += $"{i} ";
+        somaFinal += i;
+    }
+
+    Console.WriteLine($"{ordemFinal}Sum={somaFinal}");
+}
