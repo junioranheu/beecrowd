9e1e8fa1cc3fbf1d5bb5d5fafc926b959afcdece
junioranheu <junioranheu@gmail.com>
2023-03-29 23:32:13 -0300

    desafio 1115

--- 1115-quadrante/Program.cs
+++ 1115-quadrante/Program.cs
@@ -1,1 +1,29 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1115
+while (true)
+{
+    string[] quadrantes = Console.ReadLine().Split(' ');
+    int? X = int.Parse(quadrantes[0]);
+    int? Y = int.Parse(quadrantes[1]);
+
+    if (X == null || X == 0 || Y == null || Y == 0)
+    {
+        break;
+    }
+
+    if (X > 0 && Y > 0)
+    {
+        Console.WriteLine("primeiro");
+    }
+    else if (X > 0 && Y < 0)
+    {
+        Console.WriteLine("quarto");
+    }
+    else if (X < 0 && Y < 0)
+    {
+        Console.WriteLine("terceiro");
+    }
+    else if (X < 0 && Y > 0)
+    {
+        Console.WriteLine("segundo");
+    }
+}
