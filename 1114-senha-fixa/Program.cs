70e37cfe43f6067b79854c4df377baf93a54c2e0
junioranheu <junioranheu@gmail.com>
2023-03-29 23:23:54 -0300

    desafio 1114

--- 1114-senha-fixa/Program.cs
+++ 1114-senha-fixa/Program.cs
@@ -1,1 +1,13 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1114
+while (true)
+{
+    int senha = int.Parse(Console.ReadLine());
+
+    if (senha == 2002)
+    {
+        Console.WriteLine("Acesso Permitido");
+        break;
+    }
+
+    Console.WriteLine("Senha Invalida");
+}
