0c378d0c19e83a253fca5d142595d7514fda8c81
junioranheu <junioranheu@gmail.com>
2023-03-30 20:52:39 -0300

    desafio 1132

--- 1132-multiplos-de-13/Program.cs
+++ 1132-multiplos-de-13/Program.cs
@@ -1,1 +1,22 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1132
+int X = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+
+if (X > Y)
+{
+    int jar = X;
+    X = Y;
+    Y = jar;
+}
+
+int somaTotal = 0;
+
+for (int i = X; i <= Y; i++)
+{
+    if (i % 13 > 0)
+    {
+        somaTotal += i;
+    }
+}
+
+Console.WriteLine(somaTotal);
