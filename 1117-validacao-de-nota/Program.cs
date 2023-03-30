d9d2dc488e5d8e9bc422f116c18da08c8373d8f2
junioranheu <junioranheu@gmail.com>
2023-03-29 23:51:25 -0300

    desafio 1117

--- 1117-validacao-de-nota/Program.cs
+++ 1117-validacao-de-nota/Program.cs
@@ -1,1 +1,24 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1117
+int contNotasValidas = 0;
+double somaNotasValidas = 0;
+
+while (true)
+{
+    if (contNotasValidas >= 2)
+    {
+        break;
+    }
+
+    double nota = double.Parse(Console.ReadLine());
+
+    if (nota < 0 || nota > 10)
+    {
+        Console.WriteLine("nota invalida");
+        continue;
+    }
+
+    contNotasValidas++;
+    somaNotasValidas += nota;
+}
+
+Console.WriteLine($"media = {(somaNotasValidas / contNotasValidas):F2}");
