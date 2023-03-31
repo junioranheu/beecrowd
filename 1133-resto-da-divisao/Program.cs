3c70337301b48021dfb6fc229b5b8ebc228c0937
junioranheu <junioranheu@gmail.com>
2023-03-30 21:12:57 -0300

    desafio 1133

--- 1133-resto-da-divisao/Program.cs
+++ 1133-resto-da-divisao/Program.cs
@@ -1,1 +1,18 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1133
+int X = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+
+if (X > Y)
+{
+    int jar = Y;
+    Y = X;
+    X = jar;
+}
+
+for (int i = X; i < Y; i++)
+{
+    if ((i % 5 == 2 || i % 5 == 3) && (i != X))
+    {
+        Console.WriteLine(i);
+    }
+}
