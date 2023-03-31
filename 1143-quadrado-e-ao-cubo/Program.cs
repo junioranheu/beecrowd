a7c60b10053272e126f9dc760b026ab082bab651
junioranheu <junioranheu@gmail.com>
2023-03-30 21:57:25 -0300

    desafio 1143

--- 1143-quadrado-e-ao-cubo/Program.cs
+++ 1143-quadrado-e-ao-cubo/Program.cs
@@ -1,1 +1,10 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1143
+int N = int.Parse(Console.ReadLine());
+int pudim = 1;
+
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"{pudim} {Math.Pow(pudim, 2)} {Math.Pow(pudim, 3)}");
+
+    pudim++;
+}
