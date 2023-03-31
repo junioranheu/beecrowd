82567de6d3c4aae9754aa37d031a0d0a49ece9e5
junioranheu <junioranheu@gmail.com>
2023-03-30 21:48:30 -0300

    desafio 1142

--- 1142-pum/Program.cs
+++ 1142-pum/Program.cs
@@ -1,1 +1,9 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1142
+int j = int.Parse(Console.ReadLine());
+int pum = 1;
+
+for (int i = 0; i < j; i++)
+{
+    Console.WriteLine($"{pum} {pum + 1} {pum + 2} PUM");
+    pum += 4;
+}
