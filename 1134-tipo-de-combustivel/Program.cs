a99fc0baef8f4f347369a9c0dd4f5a0099b6b789
junioranheu <junioranheu@gmail.com>
2023-03-30 21:37:32 -0300

    desafio 1134

--- 1134-tipo-de-combustivel/Program.cs
+++ 1134-tipo-de-combustivel/Program.cs
@@ -1,1 +1,29 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1134
+int alcool = 0, gasolina = 0, diesel = 0;
+
+while (true)
+{
+    int resp = int.Parse(Console.ReadLine());
+
+    if (resp == 1)
+    {
+        alcool++;
+    }
+    else if (resp == 2)
+    {
+        gasolina++;
+    }
+    else if (resp == 3)
+    {
+        diesel++;
+    }
+    else if (resp == 4)
+    {
+        break;
+    }
+}
+
+Console.WriteLine("MUITO OBRIGADO");
+Console.WriteLine($"Alcool: {alcool}");
+Console.WriteLine($"Gasolina: {gasolina}");
+Console.WriteLine($"Diesel: {diesel}");
