974486c576d90080fa614338d9e516da4ff0960e
junioranheu <junioranheu@gmail.com>
2023-03-31 20:17:55 -0300

    desafio 1145

--- 1145-sequencia-logica-2/Program.cs
+++ 1145-sequencia-logica-2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1145
+string[] valores = Console.ReadLine().Split(' ');
+int X = int.Parse(valores[0]);
+int Y = int.Parse(valores[1]);
+
+for (int i = 1; i <= Y; i++)
+{
+    for (int j = 0; j < X; j++)
+    {
+        if ((i + j) > Y)
+            break;
+
+        Console.Write($"{i + (j * 1)}");
+
+        if (X - j > 1)
+            Console.Write(" ");
+    }
+
+    Console.Write("\n");
+    i += (X - 1); // Normalizar iterador;
+}
