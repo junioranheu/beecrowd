ab50ed482623e9581cbee43de72278f051c1caa8
junioranheu <junioranheu@gmail.com>
2023-03-30 22:07:34 -0300

    desafio 1144

--- 1144-sequencia-logica/Program.cs
+++ 1144-sequencia-logica/Program.cs
@@ -1,1 +1,11 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1144
+int N = int.Parse(Console.ReadLine());
+int aea = 1;
+
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"{aea} {Math.Pow(aea, 2)} {Math.Pow(aea, 3)}");
+    Console.WriteLine($"{aea} {Math.Pow(aea, 2) + 1} {Math.Pow(aea, 3) + 1}");
+
+    aea++;
+}
