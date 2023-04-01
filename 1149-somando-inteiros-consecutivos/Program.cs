35e69b7c83228c67f2f48ea5941ed9d45e70a737
junioranheu <junioranheu@gmail.com>
2023-04-01 11:20:46 -0300

    desafio 1149

--- 1149-somando-inteiros-consecutivos/Program.cs
+++ 1149-somando-inteiros-consecutivos/Program.cs
@@ -1,1 +1,16 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1149
+string[] respostas = Console.ReadLine().Split(' ');
+
+int A = int.Parse(respostas[0]);
+
+int ultimoValor = respostas.Length - 1;
+int N = int.Parse(respostas[ultimoValor]);
+
+int soma = 0;
+
+for (int i = 0; i < N; i++)
+{
+    soma += (A + i);
+}
+
+Console.WriteLine(soma);
