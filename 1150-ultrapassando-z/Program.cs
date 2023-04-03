a5685d7748c6da7dae026f737154e71c7ed54762
junioranheu <junioranheu@gmail.com>
2023-04-03 17:52:21 -0300

    desafio 1150

--- 1150-ultrapassando-z/Program.cs
+++ 1150-ultrapassando-z/Program.cs
@@ -1,1 +1,21 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1150
+int X = int.Parse(Console.ReadLine());
+int Z = int.Parse(Console.ReadLine());
+
+while (X >= Z)
+{
+    Z = int.Parse(Console.ReadLine());
+}
+
+int cont = X;
+int contTotal = 0;
+int qtdLoopsNecessariosParaChegarDoX_ao_Z = 0;
+
+while (contTotal <= Z)
+{
+    contTotal += (cont);
+    cont++;
+    qtdLoopsNecessariosParaChegarDoX_ao_Z++;
+}
+
+Console.WriteLine(qtdLoopsNecessariosParaChegarDoX_ao_Z);
