9e8abf75fc6b89a8c963d3805e539ab02dddd3b0
junioranheu <junioranheu@gmail.com>
2023-04-04 22:53:01 -0300

    desafio 1151

--- 1151-fibonacci-facil/Program.cs
+++ 1151-fibonacci-facil/Program.cs
@@ -1,1 +1,14 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1151
+int N = int.Parse(Console.ReadLine());
+
+int anterior = 0, atual = 1;
+string msg = "0 ";
+
+for (int i = 0; i < N - 1; i++)
+{
+    msg += $"{atual} ";
+    atual = atual + anterior;
+    anterior = atual - anterior;
+}
+
+Console.WriteLine(msg.TrimEnd(' '));
