038c4ed8e986e9fe63db134f1983a79bcaad23eb
junioranheu <junioranheu@gmail.com>
2023-04-04 23:26:54 -0300

    desafio 1153

--- 1153-fatorial-simples/Program.cs
+++ 1153-fatorial-simples/Program.cs
@@ -1,1 +1,11 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1153
+int N = int.Parse(Console.ReadLine());
+
+int total = N;
+
+for (int i = 1; i < N; i++)
+{
+    total *= i;
+}
+
+Console.WriteLine(total);
