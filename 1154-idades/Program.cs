502e29786b9e92992e532bb330be6af9492d45b0
junioranheu <junioranheu@gmail.com>
2023-04-06 10:35:49 -0300

    desafio 1154

--- 1154-idades/Program.cs
+++ 1154-idades/Program.cs
@@ -1,1 +1,18 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1154
+double total = 0.0;
+int qtdLoop = 0;
+
+while (true)
+{
+    int idade = int.Parse(Console.ReadLine());
+
+    if (idade < 0)
+    {
+        break;
+    }
+
+    total += idade;
+    qtdLoop++;
+}
+
+Console.WriteLine($"{(total / qtdLoop):F2}");
