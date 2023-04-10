a5219b27ae4e9c9f2f0e046fee9055725cea992f
junioranheu <junioranheu@gmail.com>
2023-04-10 19:20:13 -0300

    desafio 1156

--- 1156-sequencia-s-2/Program.cs
+++ 1156-sequencia-s-2/Program.cs
@@ -1,1 +1,15 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1156
+double S = 1.0;
+
+double primeiro = 3.0;
+double segundo = 2.0;
+
+while (primeiro <= 39)
+{
+    S += (primeiro / segundo);
+
+    primeiro += 2;
+    segundo *= 2;
+}
+
+Console.WriteLine($"{S:F2}");
