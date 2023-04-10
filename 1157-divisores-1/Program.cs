b81bad45713c393b477386674dd0ce3c585c518e
junioranheu <junioranheu@gmail.com>
2023-04-10 19:27:17 -0300

    desafio 1157

--- 1157-divisores-1/Program.cs
+++ 1157-divisores-1/Program.cs
@@ -1,1 +1,10 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1157
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    if (N % i == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
