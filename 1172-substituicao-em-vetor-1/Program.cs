876b4deb02949b0e93eb0bf3e578edf572aeadd7
junioranheu <junioranheu@gmail.com>
2023-04-11 18:07:55 -0300

    desafio 1172

--- 1172-substituicao-em-vetor-1/Program.cs
+++ 1172-substituicao-em-vetor-1/Program.cs
@@ -1,1 +1,20 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1172
+int[] X = new int[10];
+
+for (int i = 0; i < X.Length; i++)
+{
+    X[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < X.Length; i++)
+{
+    if (X[i] < 1)
+    {
+        X[i] = 1;
+    }
+}
+
+for (int i = 0; i < X.Length; i++)
+{
+    Console.WriteLine($"X[{i}] = {X[i]}");
+}
