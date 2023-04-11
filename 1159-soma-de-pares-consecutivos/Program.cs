cbd4c4c861d1815571ee3af1b51fbdb1f8d8ffaa
junioranheu <junioranheu@gmail.com>
2023-04-11 17:02:51 -0300

    desafio 1159

--- 1159-soma-de-pares-consecutivos/Program.cs
+++ 1159-soma-de-pares-consecutivos/Program.cs
@@ -1,1 +1,31 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1159
+while (true)
+{
+    int X = int.Parse(Console.ReadLine());
+
+    if (X == 0)
+    {
+        break;
+    }
+
+    int soma = 0, qtdLoops = 0;
+    const int qtdLoopsMaximo = 5;
+
+    while (true)
+    {
+        if (X % 2 == 0)
+        {
+            soma += X;
+            qtdLoops++;
+        }
+
+        if (qtdLoops == qtdLoopsMaximo)
+        {
+            break;
+        }
+
+        X++;
+    }
+
+    Console.WriteLine(soma);
+}
