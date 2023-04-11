5927d35cb638f3a1dd0ef7a1fb6de1331dac620c
junioranheu <junioranheu@gmail.com>
2023-04-11 17:57:49 -0300

    desafio 1165

--- 1165-numero-primo/Program.cs
+++ 1165-numero-primo/Program.cs
@@ -1,1 +1,28 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1165
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    int X = int.Parse(Console.ReadLine());
+
+    int qtdDivisiveis = 0;
+
+    for (int j = 1; j <= X; j++)
+    {
+        if (X % j == 0)
+        {
+            qtdDivisiveis++;
+        }
+    }
+
+    bool isPrimo = qtdDivisiveis == 2;
+
+    if (isPrimo)
+    {
+        Console.WriteLine($"{X} eh primo");
+    }
+    else
+    {
+        Console.WriteLine($"{X} nao eh primo");
+    }
+}
