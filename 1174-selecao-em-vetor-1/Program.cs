3cd0a6112bcf8e687c0a6f9c577448b6d63b394b
junioranheu <junioranheu@gmail.com>
2023-04-12 12:34:54 -0300

    desafio 1174

--- 1174-selecao-em-vetor-1/Program.cs
+++ 1174-selecao-em-vetor-1/Program.cs
@@ -1,1 +1,31 @@
-﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1174
+﻿// using System.Linq;
+
+// https://www.beecrowd.com.br/judge/pt/problems/view/1174
+double[] A = new double[100];
+string valoresMenoresOuIguais_a_10 = string.Empty;
+const char separador = ';';
+
+for (int i = 0; i < A.Length; i++)
+{
+    A[i] = double.Parse(Console.ReadLine());
+
+    if (A[i] <= 10)
+    {
+        valoresMenoresOuIguais_a_10 += $"{i}{separador}";
+    }
+}
+
+if (!string.IsNullOrEmpty(valoresMenoresOuIguais_a_10))
+{
+    string[] valoresMenoresOuIguais_a_10_double = valoresMenoresOuIguais_a_10.Split(separador).ToArray();
+
+    for (int i = 0; i < valoresMenoresOuIguais_a_10_double.Length; i++)
+    {
+        string valor = valoresMenoresOuIguais_a_10_double[i];
+
+        if (!string.IsNullOrEmpty(valor))
+        {
+            Console.WriteLine($"A[{int.Parse(valor)}] = {A[int.Parse(valor)]:F1}");
+        }
+    }
+}
