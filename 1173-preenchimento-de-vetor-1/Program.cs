7bd96071673858396b05281aa057d0cce220e3a8
junioranheu <junioranheu@gmail.com>
2023-04-12 12:14:10 -0300

    desafio 1173

--- 1173-preenchimento-de-vetor-1/Program.cs
+++ 1173-preenchimento-de-vetor-1/Program.cs
@@ -1,1 +1,22 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1173
+int[] N = new int[10];
+
+N[0] = int.Parse(Console.ReadLine()); 
+
+for (int i = 0; i < N.Length; i++)
+{
+    try
+    {
+        int anterior = N[i - 1];
+        N[i] = anterior * 2;
+    }
+    catch (Exception)
+    {
+
+    }
+}
+
+for (int i = 0; i < N.Length; i++)
+{
+    Console.WriteLine($"N[{i}] = {N[i]}");
+}
