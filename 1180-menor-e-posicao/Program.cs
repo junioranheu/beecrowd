cfa8699100f5887107886eb19f42a4d4c2285abf
junioranheu <junioranheu@gmail.com>
2023-04-22 13:52:48 -0300

    desafio 1181

--- 1180-menor-e-posicao/Program.cs
+++ 1180-menor-e-posicao/Program.cs
@@ -1,1 +1,27 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1180
+int N = int.Parse(Console.ReadLine());
+int[] X = new int[N];
+string[] valores = Console.ReadLine().Split(' ');
+
+// 1 2 3 4 -5 6 7 8 9 10
+for (int i = 0; i < X.Length; i++)
+{
+    X[i] = int.Parse(valores[i]);
+}
+
+int menorValor = X[0], posMenorValor = 0;
+
+if (X.Length > 0)
+{
+    for (int i = 0; i < X.Length; i++)
+    {
+        if (X[i] < menorValor)
+        {
+            menorValor = X[i];
+            posMenorValor = i;
+        }
+    }
+}
+
+Console.WriteLine($"Menor valor: {menorValor}");
+Console.WriteLine($"Posicao: {posMenorValor}");
--- 1181-linha-na-matriz/Program.cs
+++ 1181-linha-na-matriz/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1181
