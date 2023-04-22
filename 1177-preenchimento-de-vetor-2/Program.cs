ed98558a8a9d40fb25db832335f2cc5a0f71c2c5
junioranheu <junioranheu@gmail.com>
2023-04-22 12:59:20 -0300

    desafio 1177

--- 1177-preenchimento-de-vetor-2/Program.cs
+++ 1177-preenchimento-de-vetor-2/Program.cs
@@ -1,1 +1,15 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1177
+int T = int.Parse(Console.ReadLine());
+const int max = 1000;
+int n = 0;
+
+for (int i = 0; i < max; i++)
+{
+    Console.WriteLine($"N[{i}] = {n}");
+    n++;
+
+    if (n == T)
+    {
+        n = 0;
+    }
+}
