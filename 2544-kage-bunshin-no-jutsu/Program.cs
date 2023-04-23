6fbb8dba1da3eb6b5165505e4bbd5ecf37be33ca
junioranheu <junioranheu@gmail.com>
2023-04-23 13:40:57 -0300

    desafio 2544

---
+++ 2544-kage-bunshin-no-jutsu/Program.cs
@@ -0,0 +1,22 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2544
+// using static System.Math;
+
+while (true)
+{
+    string Nstr = Console.ReadLine();
+
+    if (Nstr == null)
+    {
+        break;
+    }
+
+    int N = int.Parse(Nstr);
+
+    if (N == 1 || N == 0)
+    {
+        Console.WriteLine(0);
+        continue;
+    }
+
+    Console.WriteLine(Math.Log(N, 2));
+}
