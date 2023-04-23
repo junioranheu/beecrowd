444cfd5eefe95cb4cd40d3499c0c09f316161b8a
junioranheu <junioranheu@gmail.com>
2023-04-23 12:52:11 -0300

    desafio 1759

--- 1759-ho-ho-ho/Program.cs
+++ 1759-ho-ho-ho/Program.cs
@@ -1,1 +1,12 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1759
+using System.Text;
+
+int N = int.Parse(Console.ReadLine());
+StringBuilder ho = new StringBuilder();
+
+for (int i = 0; i < N; i++)
+{
+    ho.Append("Ho ");
+}
+
+Console.WriteLine($"{ho.Remove(ho.Length - 1, 1)}!");
