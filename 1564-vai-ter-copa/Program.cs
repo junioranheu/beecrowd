65d3577cc4971a429894323ec896b7d405dd5736
junioranheu <junioranheu@gmail.com>
2023-04-23 12:33:20 -0300

    desafio 1564

--- 1564-vai-ter-copa/Program.cs
+++ 1564-vai-ter-copa/Program.cs
@@ -1,1 +1,20 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1564
+while (true)
+{
+    string Nstr = Console.ReadLine();
+
+    if (Nstr == null)
+    {
+        break;
+    }
+
+    int N = int.Parse(Nstr);
+
+    if (N == 0)
+    {
+        Console.WriteLine("vai ter copa!");
+    } else
+    {
+        Console.WriteLine("vai ter duas!");
+    }
+}
