936e71e034c97bd6ce2d2e1105c6c799e518c264
junioranheu <junioranheu@gmail.com>
2023-04-23 13:00:24 -0300

    desafio 1827

--- 1827-a-corrida-das-lesmas/Program.cs
+++ 1827-a-corrida-das-lesmas/Program.cs
@@ -1,1 +1,45 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1789
+while (true)
+{
+    string Lstr = Console.ReadLine();
+
+    if (Lstr == null)
+    {
+        break;
+    }
+
+    int L = int.Parse(Lstr);
+    string[] listaVelocidadesLesmas = Console.ReadLine().Split(' ');
+
+    int lesmaMaisVeloz = int.Parse(listaVelocidadesLesmas[0]);
+
+    for (int i = 0; i < L; i++)
+    {
+        if (int.Parse(listaVelocidadesLesmas[i]) > lesmaMaisVeloz)
+        {
+            lesmaMaisVeloz = int.Parse(listaVelocidadesLesmas[i]);
+        }
+    }
+
+    Console.WriteLine(DefinirNivelLesma(lesmaMaisVeloz));
+}
+
+static int DefinirNivelLesma(int lesmaMaisVeloz)
+{
+    int nivelLesmaMaisVeloz = 0;
+
+    if (lesmaMaisVeloz < 10)
+    {
+        nivelLesmaMaisVeloz = 1;
+    }
+    else if (lesmaMaisVeloz >= 10 && lesmaMaisVeloz < 20)
+    {
+        nivelLesmaMaisVeloz = 2;
+    }
+    else if (lesmaMaisVeloz >= 20)
+    {
+        nivelLesmaMaisVeloz = 3;
+    }
+
+    return nivelLesmaMaisVeloz;
+}
