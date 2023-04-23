9d7c563f9e1aa5a9ff07f59a56fc26f80b06b8ec
junioranheu <junioranheu@gmail.com>
2023-04-23 12:40:13 -0300

    desafio 1589

--- 1589-bob-conduite/Program.cs
+++ 1589-bob-conduite/Program.cs
@@ -1,1 +1,11 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1589
+int T = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < T; i++)
+{
+    string[] valores = Console.ReadLine().Split(' ');
+    int R1 = int.Parse(valores[0]);
+    int R2 = int.Parse(valores[1]);
+
+    Console.WriteLine(R1 + R2);
+}
