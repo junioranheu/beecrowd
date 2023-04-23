0d864450c870e87822f4eb22cf9eb3d6b9ab3948
junioranheu <junioranheu@gmail.com>
2023-04-23 13:31:16 -0300

    desafio 1837

--- 1837-prefacio/Program.cs
+++ 1837-prefacio/Program.cs
@@ -1,1 +1,39 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1837
+string[] valores = Console.ReadLine().Split(' ');
+int a = int.Parse(valores[0]);
+int b = int.Parse(valores[1]);
+
+int q = 0, r = 0;
+
+if (a >= 0)
+{
+    q = a / b; // Quociente = resultado da divisão;
+    r = a % b; // Resto da divisão;
+}
+else
+{
+    int x = 0, y = 0;
+
+    if (b < 0)
+    {
+        x = b * -1;
+    }
+    else
+    {
+        x = b;
+    }
+
+    for (r = 0; r < x; r++)
+    {
+        y = a - r;
+
+        if (y % b == 0)
+        {
+            break;
+        }
+    }
+
+    q = y / b;
+}
+
+Console.WriteLine($"{q} {r}");
