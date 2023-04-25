f0b5a23a5a100c7301fd74cbeec1e35ec0725ea9
junioranheu <junioranheu@gmail.com>
2023-04-25 17:25:08 -0300

    desafio 1858

--- 1858-a-resposta-de-theon/Program.cs
+++ 1858-a-resposta-de-theon/Program.cs
@@ -1,1 +1,19 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1858
+int N = int.Parse(Console.ReadLine());
+string[] valores = Console.ReadLine().Split(' ');
+
+int pos = 0 + 1;
+int menor = int.Parse(valores[0]);
+
+for (int i = 0; i < N; i++)
+{
+    int valorAtual = int.Parse(valores[i]);
+
+    if (menor > valorAtual)
+    {
+        menor = valorAtual;
+        pos = i + 1;
+    }
+}
+
+Console.WriteLine(pos);
