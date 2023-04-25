dd6da6d6d685954c44a83b42844819b121e79621
junioranheu <junioranheu@gmail.com>
2023-04-25 17:46:11 -0300

    desafio 1865

--- 1865-mjolnir/Program.cs
+++ 1865-mjolnir/Program.cs
@@ -1,1 +1,11 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1865
+int C = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < C; i++)
+{
+    string[] entrada = Console.ReadLine().Split(' ');
+    string nome = entrada[0];
+    int N = int.Parse(entrada[1]);
+
+    Console.WriteLine(nome == "Thor" ? "Y" : "N");
+}
