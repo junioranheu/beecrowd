00017f2ebfbd8079a7c774096d699121bcc7a5c0
junioranheu <junioranheu@gmail.com>
2023-04-25 17:34:17 -0300

    desafio 1864

--- 1864-nossos-dias-nunca-voltarao/Program.cs
+++ 1864-nossos-dias-nunca-voltarao/Program.cs
@@ -1,1 +1,15 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1864
+char[] caracteres = "LIFE IS NOT A PROBLEM TO BE SOLVED".ToCharArray();
+int N = int.Parse(Console.ReadLine());
+
+if (N > 0 && N <= caracteres.Length)
+{
+    string msgFinal = string.Empty;
+
+    for (int i = 0; i < N; i++)
+    {
+        msgFinal += caracteres[i];
+    }
+
+    Console.WriteLine(msgFinal);
+}
