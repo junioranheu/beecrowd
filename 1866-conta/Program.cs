67cb0643d97f3f830c00783481ae42b04cd3dc03
junioranheu <junioranheu@gmail.com>
2023-04-25 17:51:58 -0300

    desafio 1866

--- 1866-conta/Program.cs
+++ 1866-conta/Program.cs
@@ -1,1 +1,14 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1866
+int C = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < C; i++)
+{
+    int N = int.Parse(Console.ReadLine());
+
+    int qtdSoma = N % 2 == 0 ? (N / 2) : ((N / 2) + 1);
+    int qtdSubtracao = (N / 2);
+
+    int S = qtdSoma - qtdSubtracao;
+
+    Console.WriteLine(S);
+}
