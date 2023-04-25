e6d32ee3aa9d21364878489189d8f190b7968429
junioranheu <junioranheu@gmail.com>
2023-04-25 18:23:06 -0300

    desafio 1924

--- 1924-vitoria-e-a-indecisao/Program.cs
+++ 1924-vitoria-e-a-indecisao/Program.cs
@@ -1,1 +1,9 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1924
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string curso = Console.ReadLine();
+}
+
+Console.WriteLine("Ciencia da Computacao");
