a45e5d6de2641ad4d114dac1cd298d407a4b5e74
junioranheu <junioranheu@gmail.com>
2023-04-26 19:15:53 -0300

    desafio 1929

--- 1929-tomadas/Program.cs
+++ 1929-tomadas/Program.cs
@@ -1,1 +1,9 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1930
+string[] entrada = Console.ReadLine().Split(' ');
+int T1 = int.Parse(entrada[0]); 
+int T2 = int.Parse(entrada[1]);
+int T3 = int.Parse(entrada[2]);
+int T4 = int.Parse(entrada[3]);
+int T = T1 + T2 + T3 + T4 - 3;
+
+Console.WriteLine(T);
