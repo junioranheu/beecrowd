765aee2c20ce456ade79f71edcdbaca1ce4f84d6
junioranheu <junioranheu@gmail.com>
2023-04-26 21:03:34 -0300

    desafio 1959

--- 1959-poligonos-regulares-simples/Program.cs
+++ 1959-poligonos-regulares-simples/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1959
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<long> entrada = Console.ReadLine().Trim().Split(' ').Select((x) => long.Parse(x)).ToList();
+Console.WriteLine(entrada[0] * entrada[1]);
