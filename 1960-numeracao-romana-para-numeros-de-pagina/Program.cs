002e54704d0d7cff8789005100666c4742997609
junioranheu <junioranheu@gmail.com>
2023-04-27 21:51:02 -0300

    desafio 1960

--- 1960-numeracao-romana-para-numeros-de-pagina/Program.cs
+++ 1960-numeracao-romana-para-numeros-de-pagina/Program.cs
@@ -1,1 +1,35 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1960
+int N = int.Parse(Console.ReadLine());
+
+Console.Write(Converte(N / 100, "C", "D", "M"));
+N %= 100;
+Console.Write(Converte(N / 10, "X", "L", "C"));
+N %= 10;
+Console.Write(Converte(N, "I", "V", "X"));
+Console.WriteLine("");
+
+static string Converte(int digito, string um, string cinco, string dez)
+{
+    if (digito == 9)
+    {
+        return um + dez;
+    }
+    else if (digito == 4)
+    {
+        return um + cinco;
+    }
+
+    string resposta = "";
+    if (digito > 4)
+    {
+        resposta = cinco;
+        digito -= 5;
+    }
+
+    for (int i = 0; i < digito; ++i)
+    {
+        resposta += um;
+    }
+
+    return resposta;
+}
