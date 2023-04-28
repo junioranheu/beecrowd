6d5675b043006e86351d0eea437c4e5d14bae392
junioranheu <junioranheu@gmail.com>
2023-04-28 20:19:28 -0300

    desafio 1962

--- 1962-ha-muito-muito-tempo-atras/Program.cs
+++ 1962-ha-muito-muito-tempo-atras/Program.cs
@@ -1,1 +1,17 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1962
+int N = int.Parse(Console.ReadLine());
+const int doismilequinze = 2015;
+
+for (int i = 0; i < N; i++)
+{
+    int A = int.Parse(Console.ReadLine());
+ 
+    if (A >= 2015)
+    {
+        Console.WriteLine($"{(A - doismilequinze + 1)} A.C.");
+    }
+    else
+    {
+        Console.WriteLine($"{(doismilequinze - A)} D.C.");
+    }
+}
