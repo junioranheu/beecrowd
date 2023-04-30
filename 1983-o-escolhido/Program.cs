2dc19764454cb894060c1729c56b39958ecdf3c8
junioranheu <junioranheu@gmail.com>
2023-04-30 12:53:37 -0300

    desafio 1983

--- 1983-o-escolhido/Program.cs
+++ 1983-o-escolhido/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1983
+int N = int.Parse(Console.ReadLine());
+
+int melhorMatricula = 0;
+double melhorNota = 0.0;
+
+for (int i = 0; i < N; i++)
+{
+    string[] entrada = Console.ReadLine().Split(' ');
+    int matricula = int.Parse(entrada[0]);
+    double nota = double.Parse(entrada[1]);
+
+    if (nota > melhorNota)
+    {
+        melhorNota = nota;
+        melhorMatricula = matricula;
+    }
+}
+
+Console.WriteLine(melhorNota >= 8 ? melhorMatricula.ToString() : "Minimum note not reached");
