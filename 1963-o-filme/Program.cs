08c0224d68f2d27d9f920a4718b9aa953b0ddad3
junioranheu <junioranheu@gmail.com>
2023-04-29 22:23:17 -0300

    desafio 1963

--- 1963-o-filme/Program.cs
+++ 1963-o-filme/Program.cs
@@ -1,1 +1,8 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1963
+string[] entrada = Console.ReadLine().Split(' ');
+double A = double.Parse(entrada[0]);
+double B = double.Parse(entrada[1]);
+
+double aumento = ((B - A) / A) * 100;
+
+Console.WriteLine($"{aumento:F2}%");
