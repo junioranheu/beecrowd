7d28c81c72972132fa4862d714b23f30742dfabf
junioranheu <junioranheu@gmail.com>
2023-05-01 20:55:27 -0300

    desafio 1984

--- 1984-o-enigma-do-pronalandia/Program.cs
+++ 1984-o-enigma-do-pronalandia/Program.cs
@@ -1,1 +1,11 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1984
+long n = long.Parse(Console.ReadLine());
+char[] split = n.ToString().ToCharArray();   
+string reverso = "";
+
+for (long i = split.LongLength; i > 0; i--)
+{
+    reverso += split[i - 1];
+}
+
+Console.WriteLine(reverso);
