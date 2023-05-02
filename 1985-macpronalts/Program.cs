950a35bf9c77a2c31d486ccea3e97117f5c7d09b
junioranheu <junioranheu@gmail.com>
2023-05-01 21:12:49 -0300

    desafio 1985

--- 1985-macpronalts/Program.cs
+++ 1985-macpronalts/Program.cs
@@ -1,1 +1,40 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1985
+int qtd = int.Parse(Console.ReadLine());
+
+double total = 0.0;
+
+for (int i = 0; i < qtd; i++)
+{
+    string[] entradas = Console.ReadLine().Split(' ');
+    total += BuscarValor(int.Parse(entradas[0]), int.Parse(entradas[1]));
+}
+
+Console.WriteLine($"{total:F2}");
+
+static double BuscarValor(int id, int qtd)
+{
+    if (id == 1001)
+    {
+        return 1.50 * qtd;
+    }
+    else if (id == 1002)
+    {
+        return 2.50 * qtd;
+    }
+    else if (id == 1003)
+    {
+        return 3.50 * qtd;
+    }
+    else if (id == 1004)
+    {
+        return 4.50 * qtd;
+    }
+    else if (id == 1005)
+    {
+        return 5.50 * qtd;
+    }
+    else
+    {
+        return 0;
+    }
+}
