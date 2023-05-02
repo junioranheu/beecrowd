7934223e51e5284b8c7f78ecab206ed05aaf0026
junioranheu <junioranheu@gmail.com>
2023-05-02 12:41:53 -0300

    desafio 2003

--- 2003-domingo-de-manha/Program.cs
+++ 2003-domingo-de-manha/Program.cs
@@ -1,1 +1,10 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2003
+string entrada;
+
+while ((entrada = Console.ReadLine()) != null)
+{
+    int[] numeros = entrada.Trim().Split(':').Select(x => int.Parse(x)).ToArray();
+
+    int total = numeros[0] * 60 + numeros[1];
+    Console.WriteLine($"Atraso maximo: {Math.Max(0, total + 60 - 8 * 60)}");
+}
