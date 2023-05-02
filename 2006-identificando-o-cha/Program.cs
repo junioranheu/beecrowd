e1e9d5d87f9bba04d5aa3e4c501b44de36d54dc2
junioranheu <junioranheu@gmail.com>
2023-05-02 12:46:54 -0300

    desafio 2006

--- 2006-identificando-o-cha/Program.cs
+++ 2006-identificando-o-cha/Program.cs
@@ -1,1 +1,14 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2006
+int T = int.Parse(Console.ReadLine());
+string[] entrada = Console.ReadLine().Split(' ');
+int qtdOk = 0;
+
+for (int i = 0; i < entrada.Length; i++)
+{
+    if (int.Parse(entrada[i]) == T)
+    {
+        qtdOk++;
+    }
+}
+
+Console.WriteLine(qtdOk);
