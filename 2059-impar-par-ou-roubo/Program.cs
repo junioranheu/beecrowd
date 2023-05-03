e99c0bdaf36750764fae698283de105db960cfd5
junioranheu <junioranheu@gmail.com>
2023-05-03 12:43:47 -0300

    desafio 2059

--- 2059-impar-par-ou-roubo/Program.cs
+++ 2059-impar-par-ou-roubo/Program.cs
@@ -1,1 +1,29 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2059
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+List<int> entrada = Console.ReadLine().Trim().Split(' ').Select((x) => int.Parse(x)).ToList();
+
+if (entrada[3] == 1)
+{
+    if (entrada[4] == 1)
+    {
+        Console.WriteLine("Jogador 2 ganha!");
+    }
+    else
+    {
+        Console.WriteLine("Jogador 1 ganha!");
+    }
+}
+else
+{
+    if (entrada[4] == 1)
+    {
+        Console.WriteLine("Jogador 1 ganha!");
+    }
+    else
+    {
+        Console.WriteLine($"Jogador {((entrada[1] + entrada[2]) % 2 == (1 - entrada[0]) ? 1 : 2)} ganha!");
+    }
+}
