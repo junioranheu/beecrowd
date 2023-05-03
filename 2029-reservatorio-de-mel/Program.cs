b049cac708da09037f468482f45ebcfdaf829277
junioranheu <junioranheu@gmail.com>
2023-05-03 12:07:02 -0300

    desafio 2029

--- 2029-reservatorio-de-mel/Program.cs
+++ 2029-reservatorio-de-mel/Program.cs
@@ -1,1 +1,23 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2029
+const double PI = 3.14;
+
+while (true)
+{
+    string volume = Console.ReadLine();
+    string diametro = Console.ReadLine();
+
+    if (volume == null || diametro == null)
+    {
+        break;
+    }
+
+    double V = double.Parse(volume);
+    double D = double.Parse(diametro);
+    D /= 2;
+
+    double area = PI * D * D;
+    double altura = V / area;
+
+    Console.WriteLine($"ALTURA = {altura:F2}");
+    Console.WriteLine($"AREA = {area:F2}");
+}
