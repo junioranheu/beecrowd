535b3badb8478545bd53a85ada4f3f8d4a6b56da
junioranheu <junioranheu@gmail.com>
2023-05-03 12:41:57 -0300

    desafio 2057

--- 2057-fuso-horario/Program.cs
+++ 2057-fuso-horario/Program.cs
@@ -1,1 +1,27 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2057
+string[] entrada = Console.ReadLine().Split(' ');
+
+int S = int.Parse(entrada[0]);
+int T = int.Parse(entrada[1]);
+int F = int.Parse(entrada[2]);
+
+Console.WriteLine(GerarResultado(S, T, F));
+
+static int GerarResultado(int saida, int tempoViagem, int fusoHorario)
+{
+    int horarioChegada = saida + tempoViagem;
+
+    if (horarioChegada > 24)
+    {
+        horarioChegada = (24 - horarioChegada) * -1;
+    }
+
+    int resultadoFinal = horarioChegada + fusoHorario;
+
+    if (resultadoFinal < 0)
+    {
+        resultadoFinal = 24 + resultadoFinal;
+    }
+
+    return resultadoFinal;
+}
