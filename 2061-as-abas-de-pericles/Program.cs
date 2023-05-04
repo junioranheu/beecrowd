3e0d3b13a401d0a708ad202be0655adbef60ff56
junioranheu <junioranheu@gmail.com>
2023-05-04 19:44:14 -0300

    desafio 2061

--- 2061-as-abas-de-pericles/Program.cs
+++ 2061-as-abas-de-pericles/Program.cs
@@ -1,1 +1,20 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2061
+string[] entradas = Console.ReadLine().Split(' ');
+int N = int.Parse(entradas[0]); // Número inicial de abas;
+int M = int.Parse(entradas[1]); // Número de ações;
+
+for (int i = 0; i < M; i++)
+{
+    string acao = Console.ReadLine();
+
+    if (acao == "fechou")
+    {
+        N += -1 + 2;
+    }
+    else if (acao == "clicou")
+    {
+        N -= 1;
+    }
+}
+
+Console.WriteLine(N);
