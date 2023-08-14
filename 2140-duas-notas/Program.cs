4511064b9f03a3c665f6a8e690f3fc359629295b
junioranheu <junioranheu@gmail.com>
2023-08-14 19:30:20 -0300

    desafio 2140

--- 2140-duas-notas/Program.cs
+++ 2140-duas-notas/Program.cs
@@ -1,1 +1,25 @@
 ﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2140
+int[] trocos = new int[] { 4, 7, 10, 12, 15, 20, 22, 25, 30, 40, 52, 55, 60, 70, 100, 102, 105, 110, 120, 150, 200 };
+
+string entrada;
+while ((entrada = Console.ReadLine()) != null)
+{
+    List<int> numeros = entrada.Trim().Split(' ').Select(x => int.Parse(x)).ToList();
+    if (numeros[0] == 0 && numeros[1] == 0)
+    {
+        break;
+    }
+
+    bool possivel = false;
+    int troco = numeros[1] - numeros[0];
+    foreach (int possivelTroco in trocos)
+    {
+        if (troco == possivelTroco)
+        {
+            possivel = true;
+            break;
+        }
+    }
+
+    Console.WriteLine($"{(possivel ? "" : "im")}possible");
+}
