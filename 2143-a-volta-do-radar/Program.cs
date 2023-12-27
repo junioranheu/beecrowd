c0856630f7cc27090f3c47820b0c8b88459f16f7
junioranheu <junioranheu@gmail.com>
2023-12-27 15:00:58 -0300

    desafio 2143

--- 2143-a-volta-do-radar/Program.cs
+++ 2143-a-volta-do-radar/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/2143
+string entrada;
+
+while ((entrada = Console.ReadLine()) != null)
+{
+    int T = int.Parse(entrada.Trim());
+
+    for (int i = 0; i < T; ++i)
+    {
+        int N = int.Parse(Console.ReadLine().Trim());
+
+        int pontas = 2 - (N % 2);
+        Console.WriteLine(pontas + 2 * (N - pontas));
+    }
+}
